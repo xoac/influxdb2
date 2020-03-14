32686a4ec9e904ec2aae2dbeb8d15c471bedebe5
Sylwester Rąpała <sylwesterrapala@outlook.com>
2020-03-14 18:50:01 +0100

    Debug failed build

--- run_all_test.sh
+++ run_all_test.sh
@@ -1,7 +1,6 @@
 set -e
 set -x
 
-
 INFLUX_ORG=primary_org
 INFLUX_BUCKET=primary_bucket
 INFLUX_USER=user
@@ -17,5 +16,5 @@ sleep 7
 docker exec $CONTAINER influx setup -f -b $INFLUX_BUCKET -u $INFLUX_USER -p password -o $INFLUX_ORG
 sleep 1
 INFLUX_TOKEN=$(docker exec $CONTAINER influx auth list | awk 'NR==2{print $2}')
-INFLUX_ORG=$INFLUX_ORG INFLUX_TOKEN=$INFLUX_TOKEN INFLUX_BUCKET=$INFLUX_BUCKET cargo test
+INFLUX_ORG=$INFLUX_ORG INFLUX_TOKEN=$INFLUX_TOKEN INFLUX_BUCKET=$INFLUX_BUCKET cargo test --verbose --all
 docker stop $CONTAINER
